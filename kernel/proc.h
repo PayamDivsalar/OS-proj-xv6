82ab6b46f4da606b07009017e28b5868d80e50da
PayamDivsalar <payamdivsalar@aut.ac.ir>
2025-07-11 09:41:25 -0400

    thread test and a little bug fixing

--- kernel/proc.h
+++ kernel/proc.h
@@ -126,19 +126,3 @@ struct proc {
   struct thread *current_thread;  
 };
 
-enum threadstate {
-  THREAD_UNUSED,
-  THREAD_RUNNABLE,
-  THREAD_RUNNING,
-  THREAD_JOINED,
-  THREAD_SLEEPING
-};
-
-struct thread {
-  enum threadstate state;      
-  struct trapframe *trapframe;  
-  uint id;                     
-  uint join;                 
-  int sleep_n;                 
-  uint sleep_tick0;         
-};
